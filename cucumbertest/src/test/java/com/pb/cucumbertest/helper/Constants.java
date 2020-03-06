1c137dbe2d7256c80f68c5428136c15f564d14ef
tgoswami <goswami.tarun77@gmail.com>
2020-03-06 12:56:49 +0530

    test

--- cucumbertest/src/test/java/com/pb/cucumbertest/helper/Constants.java
+++ cucumbertest/src/test/java/com/pb/cucumbertest/helper/Constants.java
@@ -5,4 +5,6 @@ public class Constants
 	// Add all constants here
 	public final static String MAC_CHROME_DRIVER = System.getProperty("user.dir")+"//drivers//chromedriver";
 	public final static String EXTENT_CONFIG_PATH = System.getProperty("user.dir")+"//com//pb//cucumbertest//util//extent-config.xml";
+	//Test
 }
+
