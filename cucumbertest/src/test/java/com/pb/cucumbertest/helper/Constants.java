09db59e4caeb35427e0c5cfc61335a4b295822dc
admin <admin@192.168.50.1>
2020-07-20 22:15:39 +0530

    my first commit000000

--- cucumbertest/src/test/java/com/pb/cucumbertest/helper/Constants.java
+++ cucumbertest/src/test/java/com/pb/cucumbertest/helper/Constants.java
@@ -6,5 +6,8 @@ public class Constants
 	public final static String MAC_CHROME_DRIVER = System.getProperty("user.dir")+"//drivers//chromedriver";
 	public final static String EXTENT_CONFIG_PATH = System.getProperty("user.dir")+"//com//pb//cucumbertest//util//extent-config.xml";
 	//Test
+	
+	int a;
+	
 }
 
